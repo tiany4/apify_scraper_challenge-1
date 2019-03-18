dde603ba5adbfa5133ba8fe00c135b1bbb741bc2
Yuchen Tian <14116340+tiany4@users.noreply.github.com>
2019-03-18 07:38:21 -0400

    part 1-3 done

--- main.js
+++ main.js
@@ -7,23 +7,53 @@ Apify.main(async () => {
 
     // Get queue and enqueue first url.
     const requestQueue = await Apify.openRequestQueue();
-    //await requestQueue.addRequest(new Apify.Request({url: 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/'}));
+    await requestQueue.addRequest(new Apify.Request({url: 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/'}));
     await requestQueue.addRequest(new Apify.Request({url: 'https://www.visithoustontexas.com/events'}));
     // url to grab event until end of 2019
     // https://www.visithoustontexas.com/events/?endDate=12%2F31%2F2019
 
     const crawler = new Apify.PuppeteerCrawler({
-        maxRequestsPerCrawl: 4,
+        //maxRequestsPerCrawl: 4,
         requestQueue,
 
         // This page is executed for each request.
         // If request failes then it's retried 3 times.
         // Parameter page is Puppeteers page object with loaded page.
         handlePageFunction: async ({page, request}) => {
-            // this if statement is here purely for part 1
+
             if (await page.url() === 'https://www.visithoustontexas.com/events/') {
-                await getEventList({page, request});
-            } else await getEventData({page, request});
+                let file = fs.createWriteStream('events.txt');
+                hasNextPage = true;
+
+                while (hasNextPage) {
+                    // grab event links into array
+                    listOfLinks = await page.evaluate(() => {
+                        const tempTitles = Array.from(document.querySelectorAll('.eventItem div.title a'));
+                        return tempTitles.map(el => el.href);
+                    });
+
+                    // write to file and console
+                    file.on('error', function (err) {
+                        throw error;
+                    });
+                    listOfLinks.forEach(async function(link) {
+                        file.write(link + '\n');
+                        await requestQueue.addRequest(new Apify.Request({url: link}));
+                    });
+                    console.log(listOfLinks[0]);
+
+                    // check if there are more pages
+                    if (await page.$('.sharedPagerContainer a.arrow.next.disabled') === null) {
+                        await page.click('.sharedPagerContainer a.arrow.next');
+                        // todo: use waitForNavigation instead of band-aid fix
+                        await page.waitFor(1500);
+                    } else hasNextPage = false;
+                }
+
+                file.end();
+            } else
+                // this is here purely for part 1
+                await getEventData({page, request});
         },
 
         // If request failed 4 times then this function is executed.
@@ -44,10 +74,9 @@ const getEventData = async ({page, request}) => {
     let adr = await page.$eval('.adrs', el => el.innerText);
     const adrSplit = adr.split('|');
     const street = adrSplit[0].trim();
-    const adrSplitTwo = adrSplit[1].split(',');
-    const city = adrSplitTwo[0].trim();
-    const state = adrSplitTwo[1].trim().substring(0, 2);
-    const zip = adrSplitTwo[1].trim().substring(adrSplitTwo[1].length - 6);
+    const city = adrSplit[1].trim().substring(0, adrSplit[1].length - 9);
+    const state = adrSplit[1].trim().substring(adrSplit[1].length - 9, adrSplit[1].length - 7);
+    const zip = adrSplit[1].trim().substring(adrSplit[1].length - 6);
 
     // parse event details without classes
     // store list of details in an array
@@ -57,10 +86,10 @@ const getEventData = async ({page, request}) => {
     });
     const infoList = infoListToParse[0].split('\n');
 
-    var contact, times, phone, recurring, admission, date, title, timestamp;
+    let contact, times, phone, recurring, admission, date, title, timestamp;
     title = await page.title();
-    titleSplit = title.split('|');
-    const description = titleSplit[0];
+    let titleSplit = title.split('|');
+    let description = titleSplit[0].trim();
     timestamp = new Date().toISOString();
 
     // logic to parse each line
@@ -86,12 +115,23 @@ const getEventData = async ({page, request}) => {
 
     // parse date and time with momentjs
     date = await page.$eval('.dates', el => el.innerText);
-    const dateSplit = date.split('-');
-    const timeSplit = times.split(' to ');
-    let startDate = dateSplit[0].trim().replace(',', '');
-    let endDate = dateSplit[1].trim().replace(',', '');
-    startDate = moment.parseZone(startDate + ' ' + timeSplit[0], 'MMM DD YYYY hh:mm a').toISOString();
-    endDate = moment.parseZone(endDate + ' ' + timeSplit[1], 'MMM DD YYYY hh:mm a').toISOString();
+    let startDate, endDate;
+    if (date.includes('-')) {
+        const dateSplit = date.split('-');
+        startDate = dateSplit[0].trim().replace(',', '');
+        endDate = dateSplit[1].trim().replace(',', '');
+    } else {
+        startDate = date.replace(',', '');
+        endDate = startDate;
+    }
+    if (times != null) {
+        const timeSplit = times.split(' to ');
+        startDate = moment.parseZone(startDate + ' ' + timeSplit[0], 'MMM DD YYYY hh:mm a').toISOString();
+        endDate = moment.parseZone(endDate + ' ' + timeSplit[1], 'MMM DD YYYY hh:mm a').toISOString();
+    } else {
+        startDate = moment.parseZone(startDate, 'MMM DD YYYY').toISOString();
+        endDate = moment.parseZone(endDate, 'MMM DD YYYY').toISOString();
+    }
 
     // save details in an object
     let event = {
@@ -123,6 +163,7 @@ const getEventData = async ({page, request}) => {
     // save event details to file
     // remove special characters from timestamp
     timestamp = timestamp.replace(/[^\w\s]/g, '_');
+    description = description.replace(/[^\w\s]/g, '_');
 
     let filename = description + timestamp + '.json';
     fs.writeFile(filename, JSON.stringify(event), (err => {
@@ -131,32 +172,4 @@ const getEventData = async ({page, request}) => {
     }));
 
     // Occasional error: process cannot be terminated; No noticeable bug currently
-};
-
-const getEventList = async ({page, request}) => {
-    let file = fs.createWriteStream('events.txt');
-    hasNextPage = true;
-
-    while (hasNextPage) {
-        listOfTitles = await page.evaluate(() => {
-            const tempTitles = Array.from(document.querySelectorAll('.eventItem div.title a'));
-            return tempTitles.map(el => el.href);
-        });
-
-        // write to file and console
-        file.on('error', function (err) {
-            throw error;
-        });
-        listOfTitles.forEach(function(link) {
-            file.write(link + '\n');
-        });
-        console.log(listOfTitles);
-
-        if (await page.$('.sharedPagerContainer a.arrow.next.disabled') === null) {
-            await page.click('.sharedPagerContainer a.arrow.next');
-            // todo: use waitForNavigation instead of band-aid fix
-            await page.waitFor(1500);
-        } else hasNextPage = false;
-    }
-    file.end();
 };
